e058e72c49f4fae943e0d901f562c56a9daddefe
terryt925 <thindterry@yahoo.com>
2021-07-01 14:38:28 -0700

    Update store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
+
 const configureStore = (preloadedState = {}) => (createStore(
   rootReducer, preloadedState, applyMiddleware(thunk))
 );
