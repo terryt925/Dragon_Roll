707b440888df6a1c5d407be818ff6f7f17a2b655
terryt925 <thindterry@yahoo.com>
2021-08-25 15:01:59 -0700

    Update route util

--- frontend/utils/route_util.jsx
+++ frontend/utils/route_util.jsx
@@ -12,7 +12,6 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
   )} />
 );
 
-
 const Protected = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
     loggedIn ? (
